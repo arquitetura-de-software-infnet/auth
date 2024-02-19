19cc907ddf11a576018bff03d2d18c15208c9301
Paulo Machado <84181965+paulomachadodev@users.noreply.github.com>
2024-02-19 20:59:30 -0300

    First commit

---
+++ src/main/java/com/store/auth/configuration/SwaggerConfig.java
@@ -0,0 +1,7 @@
+package com.store.auth.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+}
---
+++ src/main/java/com/store/auth/domain/User.java
@@ -0,0 +1,20 @@
+package com.store.auth.domain;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property="id")
+public class User {
+}
